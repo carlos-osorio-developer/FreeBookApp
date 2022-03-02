b60dcb2accee8d367505168427aa3bf0ac370f13
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-02 18:57:14 -0500

    Add global styling for the app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import List from './components/list';
 import Categories from './pages/categories';
+import './app.css';
 
 export default function App() {
   const books = [
