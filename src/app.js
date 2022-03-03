e8b99b1401b5680cf5afcb30613c4349bbd781a7
carlos-osorio-developer <osorio.uis@gmail.com>
2022-03-03 15:52:04 -0500

    Add setters and getters inside components with Redux State

--- src/app.js
+++ src/app.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Header from './components/header';
 import List from './components/list';
 import Categories from './pages/categories';
 import './app.css';
 
 export default function App() {
-  const books = [
-    {
-      id: 1,
-      title: 'The Lord of the Rings',
-      author: 'J.R.R. Tolkien',
-      category: 'Fantasy',
-      completed: 0,
-      chapter: 1,
-    },
-    {
-      id: 2,
-      title: 'The Hobbit',
-      author: 'J.R.R. Tolkien',
-      category: 'Fantasy',
-      completed: 30,
-      chapter: 2,
-    },
-  ];
-
+  const books = useSelector((state) => state.books);
   return (
     <div className="app">
       <Header />
--- src/components/newForm.js
+++ src/components/newForm.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
 export default function NewForm() {
+  const dispatch = useDispatch();
+
+  const dispatchNewBook = (e) => {
+    e.preventDefault();
+    const book = {
+      id: Date.now(),
+      title: 'first',
+      author: 'first',
+      category: 'first',
+      completed: 0,
+      chapter: 0,
+    };
+    dispatch(addBook(book));
+  };
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={dispatchNewBook}>
       <input type="text" placeholder="Title" className="form-title" />
       <input type="text" placeholder="Author" className="form-author" />
       <input type="text" placeholder="Category" className="form-category" />
